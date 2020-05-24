46491e5d9aa41a7b0d531c58c425837d73098ecc
chopokmado <chopokmado@gmail.com>
2020-05-24 17:11:59 +0900

    주석 추가 테스트

--- src/main/java/nmayorov/app/StartDemoServer.java
+++ src/main/java/nmayorov/app/StartDemoServer.java
@@ -9,6 +9,11 @@ import java.net.InetSocketAddress;
 class StartDemoServer {
     private static final int PORT = 5000;
 
+    /**
+     * 서버 데몬의 시작
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         NioServer server = new NioServer(new InetSocketAddress(PORT));
         Chat chat = new Chat(server, 10, 100);
